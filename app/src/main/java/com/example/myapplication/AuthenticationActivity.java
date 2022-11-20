2001d31b4eabbbea0c82c68ef2a36cc26bbc1a81
dvphuocnguyen <75025465+dvphuocnguyen@users.noreply.github.com>
2022-11-20 10:32:10 +0700

    connect

--- app/src/main/java/com/example/myapplication/AuthenticationActivity.java
+++ app/src/main/java/com/example/myapplication/AuthenticationActivity.java
@@ -1,10 +1,10 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.databinding.ActivityAuthenticationBinding;
 import com.example.myapplication.databinding.ActivityAuthenticationBinding;
@@ -18,70 +18,73 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class AuthenticationActivity extends AppCompatActivity {
     ActivityAuthenticationBinding binding;
-    String name, email, password;
+    String name,email,password;
     DatabaseReference databaseReference;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityAuthenticationBinding.inflate(getLayoutInflater());
+        binding=ActivityAuthenticationBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        databaseReference= FirebaseDatabase.getInstance().getReference("users");
 
         binding.login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                email = binding.email.getText().toString();
-                password = binding.pasword.getText().toString();
+                email=binding.email.getText().toString();
+                password=binding.pasword.getText().toString();
                 login();
             }
         });
         binding.signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                name = binding.name.getText().toString();
-                email = binding.email.getText().toString();
-                password = binding.pasword.getText().toString();
+                name=binding.name.getText().toString();
+                email=binding.email.getText().toString();
+                password=binding.pasword.getText().toString();
+
                 signUp();
             }
         });
+
     }
 
-    protected void onStart(){
+    @Override
+    protected void onStart() {
         super.onStart();
-        if(FirebaseAuth.getInstance().getCurrentUser()!=null){
-            startActivity(new Intent(AuthenticationActivity.this, MainActivity.class));
+        if (FirebaseAuth.getInstance().getCurrentUser()!=null){
+            startActivity(new Intent(AuthenticationActivity.this,MainActivity.class));
             finish();
         }
     }
 
-
     private void login() {
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email.trim(), password)
+        FirebaseAuth
+                .getInstance()
+                .signInWithEmailAndPassword(email.trim(),password)
                 .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                     @Override
                     public void onSuccess(AuthResult authResult) {
-                        startActivity(new Intent(AuthenticationActivity.this, MainActivity.class));
+                        startActivity(new Intent(AuthenticationActivity.this,MainActivity.class));
                         finish();
                     }
                 });
     }
-    private void signUp(){
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email.trim(), password)
+    private void signUp() {
+        FirebaseAuth
+                .getInstance()
+                .createUserWithEmailAndPassword(email.trim(),password)
                 .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                     @Override
                     public void onSuccess(AuthResult authResult) {
-                        UserProfileChangeRequest userProfileChangeRequest = new UserProfileChangeRequest.Builder().setDisplayName(name).build();
-                        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                        UserProfileChangeRequest userProfileChangeRequest=new UserProfileChangeRequest.Builder().setDisplayName(name).build();
+                        FirebaseUser firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
                         firebaseUser.updateProfile(userProfileChangeRequest);
-                        startActivity(new Intent(AuthenticationActivity.this, MainActivity.class));
+                        UserModel userModel=new UserModel(FirebaseAuth.getInstance().getUid(),name,email,password);
+                        databaseReference.child(FirebaseAuth.getInstance().getUid()).setValue(userModel);
+                        startActivity(new Intent(AuthenticationActivity.this,MainActivity.class));
                         finish();
-
                     }
                 });
     }
-
-
-
-
-
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,20 +6,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.myapplication.databinding.ActivityAuthenticationBinding;
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 
 public class MainActivity extends AppCompatActivity {
-
-
+    FirebaseFirestore  firestore;
+    //ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
         setContentView(R.layout.activity_main);
 
+        firestore = FirebaseFirestore.getInstance();
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("firstName", "Phuoc");
+        user.put("lastName", "Nguyen");
+        user.put("des","test");
+
+        firestore.collection("user").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Fail", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
